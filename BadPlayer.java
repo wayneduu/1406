5ec7848fadd7fa486d7a4e295dffaa36ff9f7590
wayneduu <waynedu@cmail.carleton.ca>
2017-08-16 18:12:21 -0400

    1812
    1812

--- BadPlayer.java
+++ BadPlayer.java
@@ -4,17 +4,19 @@ import java.util.Stack;
 
 public class BadPlayer extends Player{
 
-	public BadPlayer(Card[] cards){this.hand = new ArrayList<Card>(Arrays.asList(cards));}
+ public BadPlayer(Card[] cards){this.hand = new ArrayList<Card>(Arrays.asList(cards));}
  
   /* play a card */ 
-	public boolean play(DiscardPile       discardPile, 
-	                    Stack<Card>       drawPile, 
-											ArrayList<Player> players)
-	{
-		discardPile.add(this.hand.remove(0));
-		if( this.hand.size() == 0 ){return true;}
-		return false;
-	}
-	
-	
+ public boolean play(DiscardPile       discardPile,
+                                 Stack<Card>       drawPile,
+                                 ArrayList<Player> players,
+                                 int player,
+                                 boolean direction)
+ {
+  discardPile.add(this.hand.remove(0));
+  if( this.hand.size() == 0 ){return true;}
+  return false;
+ }
+ 
+ 
 }
